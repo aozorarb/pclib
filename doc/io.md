d343ef0747e921dc8acfbf07a634f0b93e399ea1
aozorarb <melon0802yota800@icloud.com>
2023-03-21 18:57:19 +0900

    add io

---
+++ doc/io.md
@@ -0,0 +1,3 @@
+    ** puts(vector vec) **
+vecの中身をスペース区切りで出力し改行する。
+ruby: puts vec.join(" ")
---
+++ io.hpp
@@ -0,0 +1,18 @@
+#ifndef IO_HPP
+#define IO_HPP 1
+
+#include <iostream>
+#include <vector>
+
+namespace procon {
+  template <class T>
+
+  void puts(const vector<T> &vec) {
+    size_t size = vec.size();
+    if(size == 0) return;
+    for(int i=0;i<size;++i) {
+      cout << vec[i] << " \n"[(i == size-1)];
+    }
+}
+
+#endif // IO_HPP
