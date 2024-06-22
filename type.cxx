6ea591ac69d29f2ccdc1426bd54fe0d37312a579
aozorarb <62017558+aozorarb@users.noreply.github.com>
2024-06-22 16:11:28 +0900

    Create type.cxx. define type to often use vector

---
+++ type.cxx
@@ -0,0 +1,5 @@
+using vec_int = std::vector<int>;
+using vec2_int = std::vector<std::vector<int> >;
+
+using vec_str = std::vector<std::string>;
+using vec2_str = std::vector<std::vector<std::string> >;
